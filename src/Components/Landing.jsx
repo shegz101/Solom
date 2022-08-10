6e078724251bcaa16d70acf7f87117eae81fd112
shegz101 <66688833+shegz101@users.noreply.github.com>
2022-08-10 23:36:48 +0100

    landing-page-all-set

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import Header from './Header';
+import nft from '../solnftbg.png';
 import '../Styles/Landing.css';
 
 const Landing = () => {
     return (
         <div className='landing__section'>
             <Header/>
-            <div>
+            <div className='landing__row'>
                 <div>
                     <h1 className='intro__text'>Discover and Connect Your deligent Fans</h1>
-                    <p className='intro__desc'>Let your most passionate fans support your creative work via monthly membership.</p>
+                    <p className='intro__descp'>Let your most passionate fans support your creative work via monthly membership.</p>
                 </div>
-                <div></div>
+                <div>
+                    <span><img src={nft} className="nft__img" alt='nft-img'/></span>
+                </div>
+            </div>
+
+            <div className='grp__btn'>
+                <button className='explore__btn'>Explore</button>
+                <button className='collection__btn'>Create Collection</button>
             </div>
         </div>
     )
