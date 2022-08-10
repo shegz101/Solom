bf199804149f6d2a32c0324efd6abc78792ce7c4
shegz101 <66688833+shegz101@users.noreply.github.com>
2022-08-10 18:15:02 +0100

    things-work-well

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
     return (
         <div className="header__section">
             <div className='header__logo'>
-                <span><img src={logo} style={{fontSize:'20px',height:'29px',marginTop:'25px',borderRadius:'15px',}} alt="solomon-logo"/></span>
-                <h1 style={{color:'white',paddingLeft:'10px', cursor:'pointer',}} className='header-text'>Solomon</h1>
+                <span><img src={logo} style={{fontSize:'20px',height:'45px',marginTop:'25px',borderRadius:'15px',}} alt="solomon-logo"/></span>
+                <h1 style={{color:'white',paddingLeft:'10px', paddingTop:'8px', cursor:'pointer',}} className='header-text'>Solomon</h1>
             </div>
 
             <div>
                 <nav className='nav__links'>
-                    <ul style={{color:'white', fontSize:'25px'}}>Home</ul>
-                    <ul style={{color:'white', fontSize:'25px'}}>Artists & Creators</ul>
-                    <ul style={{color:'white', fontSize:'25px'}}>Your Profile</ul>
+                    <ul style={{color:'white', fontSize:'20px'}}>Home</ul>
+                    <ul style={{color:'white', fontSize:'20px'}}>Artists & Creators</ul>
+                    <ul style={{color:'white', fontSize:'20px'}}>Your Profile</ul>
                 </nav>
             </div>
 
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import Header from './Header';
+import '../Styles/Landing.css';
 
 const Landing = () => {
     return (
-        <Header/>
+        <div className='landing__section'>
+            <Header/>
+            <div>
+                <div>
+                    <h1 className='intro__text'>Discover and Connect Your deligent Fans</h1>
+                    <p className='intro__desc'>Let your most passionate fans support your creative work via monthly membership.</p>
+                </div>
+                <div></div>
+            </div>
+        </div>
     )
 }
 
