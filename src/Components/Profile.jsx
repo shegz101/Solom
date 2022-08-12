6f535ccf5469d431a74a88706a0c0e772df6cc7c
shegz101 <66688833+shegz101@users.noreply.github.com>
2022-08-12 18:23:13 +0100

    profile-page-updated

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import '../Styles/Profile.css';
 import { Link } from "react-router-dom";
 import Header from './Header';
@@ -8,15 +8,21 @@ const Profile = () => {
     const [item, setItem] = useState(false);
     const [collection, setCollection] = useState(false);
     const [activity, setActivity] = useState(false);
+    const textAreaRef = useRef(null);
     const address = '4fJScLXQYafWCfRS29t6EsqhTH3RCeZaRsWFCwidsQAQ';
-    const minimize = `${address?.slice(0, 6)}..${address?.slice(-4)}` ;
+    const minimize = `${address?.slice(0, 6)}..${address?.slice(-4)}`;
+
+    const copyToClipboard = () => {
+        textAreaRef.current.select();
+        document.execCommand('copy');
+    }
     return (
         <div className='profile__section'>
             <div style={{marginRight:'40px',}}><Header /></div>
            <div className='profile__img'>
               <div className='img__div'></div>
               <div className='address__edit' style={{marginTop:'-13px'}}>
-                <p><span style={{color:'white',}}>{minimize}</span> <span style={{marginTop:'60px'}}><MdContentCopy style={{color:'#3e00b3',}}/></span></p>
+                <p><span style={{color:'white',}} ref={textAreaRef}>{minimize}</span> <span style={{}} onClick={copyToClipboard}><MdContentCopy style={{color:'#3e00b3',paddingTop:'50px'}}/></span></p>
                 <Link to="/signup" style={{textDecoration:'none', outline:'none',}}><button style={{backgroundColor:'#17076C',outline:'none', textDecoration:'none', border:'1px solid transparent', color:'white', marginTop:'-10px', height:'30px'}}>Edit profile</button></Link>
               </div>
               <div>
@@ -29,9 +35,6 @@ const Profile = () => {
            </div>
 
            <div clasName='activities__space'>
-              {
-                
-              }
               {
                 item && (
                     <p style={{color:'white',}}>You have no items yet</p>
