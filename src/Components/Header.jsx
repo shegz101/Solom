dba78198f4b455e3e1cf986c155b14b472fc7200
shegz101 <66688833+shegz101@users.noreply.github.com>
2022-08-14 04:08:26 +0100

    updated-web-app-name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <div className="header__section">
             <div className='header__logo'>
                 <span><img src={logo} style={{fontSize:'20px',height:'45px',marginTop:'25px',borderRadius:'15px',}} alt="solomon-logo"/></span>
-                <Link to="/" style={{textDecoration:'none', outline:'none',}}><h1 style={{color:'white',paddingLeft:'10px', paddingTop:'8px', cursor:'pointer',}} className='header-text'>Solomon</h1></Link>
+                <Link to="/" style={{textDecoration:'none', outline:'none',}}><h1 style={{color:'white',paddingLeft:'10px', paddingTop:'8px', cursor:'pointer',}} className='header-text'>Soltribe</h1></Link>
             </div>
 
             <div>
